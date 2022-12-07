e85f0deed4b248220f0fdb3147ab3d69bcbdb024
byryu <90186249+RYOOBOYOON@users.noreply.github.com>
2022-12-07 17:53:46 +0900

    css module tutorial

--- styling-with-sass/src/App.tsx
+++ styling-with-sass/src/App.tsx
@@ -1,14 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import Button from "./Components/Button";
+import CheckBox from "./Components/CheckBox";
 
 function App() {
+  const [check, setCheck] = useState(false);
+  const onChange = (e: any) => {
+    setCheck(e.target.checked);
+  };
   return (
     <div className="App">
+      <CheckBox onChange={onChange} checked={check}>
+        다음 약관에 모두 동의
+      </CheckBox>
+      <p>
+        <b>check: </b>
+        {check ? "true" : "false"}
+      </p>
       <div className="buttons">
-        <Button size="large">BUTTON</Button>
+        {/* <Button size="large" onClick={() => console.log("클릭됐다!")}>
+          BUTTON
+        </Button> */}
         <Button>BUTTON</Button>
         <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="gray">
+          BUTTON
+        </Button>
+        <Button color="gray">BUTTON</Button>
+        <Button size="small" color="gray">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="pink">
+          BUTTON
+        </Button>
+        <Button color="pink">BUTTON</Button>
+        <Button size="small" color="pink">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" outline>
+          BUTTON
+        </Button>
+        <Button color="gray" outline>
+          BUTTON
+        </Button>
+        <Button size="small" color="pink" outline>
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth>
+          BUTTON
+        </Button>
+        <Button color="gray" outline fullWidth>
+          BUTTON
+        </Button>
+        <Button size="small" color="pink" fullWidth>
+          BUTTON
+        </Button>
       </div>
     </div>
   );
--- styling-with-sass/src/Components/Button.tsx
+++ styling-with-sass/src/Components/Button.tsx
@@ -1,19 +1,20 @@
-import React from "react";
 import classNames from "classnames";
 import "./Button.scss";
 
-function Button({
-  children,
-  size,
-}: {
-  children: React.ReactNode;
-  size: string;
-}) {
-  return <button className={classNames("Button", size)}>{children}</button>;
+function Button({ children, size, color, outline, fullWidth, ...rest }: any) {
+  return (
+    <button
+      className={classNames("Button", size, color, { outline, fullWidth })}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 }
 
 Button.defaultProps = {
   size: "medium",
+  color: "blue",
 };
 
 export default Button;
